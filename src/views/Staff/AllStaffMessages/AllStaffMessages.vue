560b801851ab21414cd46cee4ac2eb11d3dad982
akbarsaladin36 <asong36@gmail.com>
2022-07-31 00:03:45 +0700

    update page in all messages for staff role

--- src/views/Staff/AllStaffMessages/AllStaffMessages.vue
+++ src/views/Staff/AllStaffMessages/AllStaffMessages.vue
@@ -2,11 +2,61 @@
   <div class="mt-5 text-center">
       <h1>All Messages</h1>
   </div>
+
+      <router-link to="/buyer/all-messages/create" class="btn btn-primary mt-5">New Message</router-link>
+
+    <div class="mt-5">
+    <table class="table table-bordered border-dark">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Message Title</th>
+          <th>Sent To</th>
+          <th>Message Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(message,index) in allStaffMessages" :key="index++">
+          <td>{{ index }}</td>
+          <td>{{ message.message_title }}</td>
+          <td>{{ message.receiver_id }}</td>
+          <td>
+            <div v-if="message.message_status === 'received' ">
+              <a href="#" class="btn btn-success">Reply</a>
+            </div>
+            <div v-else>
+              {{ message.message_status }}
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
+import axiosApiIntances from '../../../utils/axios'
 export default {
-    name: 'AllStaffMessages'
+    name: 'AllStaffMessages',
+    data() {
+      return {
+        allStaffMessages: []
+      }
+    },
+    methods: {
+      handleGetAllStaffMessages() {
+        axiosApiIntances.get('messages')
+        .then((res)=>{
+          this.allStaffMessages = res.data.data
+        })
+        .catch((err)=>{
+          console.log(err)
+        })
+      }
+    },
+    created() {
+      this.handleGetAllStaffMessages()
+    }
 }
 </script>
 
